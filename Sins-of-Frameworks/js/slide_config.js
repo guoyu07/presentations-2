198be95164ce959b766672179efd953ed4834921
GaRY <gary@geeker.im>
2013-03-15 18:13:05 +0800

    init Sins-of-Frameworks

---
+++ Sins-of-Frameworks/js/slide_config.js
@@ -0,0 +1,29 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: '开发框架的原罪',
+    subtitle: 'The Sins of Frameworks',
+    eventTitle: '一号店安全沙龙',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/coding.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'GaRY (aka wofeiwo)',
+    company: '安全工程师 @PPS @Baidu @80sec',
+    weibo: 'wofeiwo',
+    www: 'http://www.80sec.com',
+    github: 'https://github.com/wofeiwo'
+  }]
+};
+
